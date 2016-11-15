71ada60f1102dd3de86b1f96c2e864aac570beb2
Sam Levan <sam@madkudu.com>
2016-11-14 23:38:39 -0800

    add a grunt file

---
+++ front/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'bundle_babel.js',
+        dest: 'bundle.min.js'
+      }
+    },
+		browserify: {
+			client: {
+				src: ['index.js'],
+				dest: 'bundle.js'
+			}
+		},
+		babel: {
+        options: {
+            sourceMap: true,
+            presets: ['latest']
+        },
+        dist: {
+            files: {
+                'bundle_babel.js': './bundle.js'
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-babel');
+
+  // Default task(s).
+	// grunt.registerTask('default', ['babel']);
+	// grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['browserify', 'babel', 'uglify']);
+
+};
